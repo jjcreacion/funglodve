3b84b59e63cbbe52db0e24520d6ec6b7b58f00df
Johann <jjcreacion@gmail.com>
2024-10-12 09:47:43 -0400

    12/10/2024

--- dashboard.php
+++ dashboard.php
@@ -1,96 +1,90 @@
-<!-- <!doctype html>
+<!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link rel="stylesheet" href="lib/css/Styles.css">
+    <link rel="stylesheet" href="./lib/css/Styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
-  </head>
+</head>
   <body>
   <div class='dashboard'>
-        <div class="dashboard-nav">
-            <header>
-            <a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a>
-            <a href="#" class="brand-logo"><i class="fas fa-anchor"></i> </a>
-            <img src="lib/img/logovertical.png" width="130" height="120">
-           </header>
-            <nav class="dashboard-nav-list">
-              
-              <a href="#" class="dashboard-nav-item">
-              <i class="fa-solid fa-medal"></i>
-               Glorias
-              </a>
-              <a href="#" class="dashboard-nav-item active">
-              <i class="fas fa-sitemap"></i>
-                Subsedes
-              </a>
-              <a href="#" class="dashboard-nav-item">
-               <i class="fas fa-hotel"></i>
-                Instituciones 
-               </a>
-               <a href="#" class="dashboard-nav-item">
-               <i class="fas fa-chart-bar"></i>
-                Proyectos
-              </a>
-              <a href="#" class="dashboard-nav-item">
-              <i class="fas fa-chart-pie"></i>
-                Reportes
-              </a>
-              <a href="#" class="dashboard-nav-item"><i class="fas fa-file-invoice"></i>
-                Nominas 
-               </a>
-               <a href="#" class="dashboard-nav-item"><i class="fas fa-user-tie"></i>
-                Usuarios 
-               </a>
-            <div class="nav-item-divider"></div>
-            </nav>
-        </div>
+        <?php
+        $activePage = '';
+        require_once('./views/components/navbar.php');
+        ?>
         <div class='dashboard-app'>
-            <header class='dashboard-toolbar'><a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a></header>
-            <header class='dashboard-toolbar'>
-                <a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a>
-                <span><h2 class="tituloDashboar">Fundación Glorias Deportivas de Venezuela</h2></span>
-                <a href="#!" class="menu-toggle"><i class="fas fa-sign-out-alt"></i></a>
-            </header>
+           <?php
+            require_once('./views/components/header.php');    
+            ?>
+            <div class='dashboard-content'>
+                <div class='container'>
+                <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-md-6">
+                             <div class="card">
+                             <img src="./lib/img/logo1.png" alt="Logo" class="logo">
+                             </div>
+                        </div>
+                        <div class="col-md-6">
+                        <div class="card">
+                              <div class="card-header">
+                               <i class="fa-solid fa-cake-candles"></i>
+                                  Cumpleaños del día
+                              </div>
 
-            
+                              <table class="table table-striped">
+                                <tbody>
+                                  <tr>
+                                    <th scope="row">1</th>
+                                    <td>José Perez</td>
+                                    <td>Táchira</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">2</th>
+                                    <td>Juan Rodríguez</td>
+                                    <td>Sucre</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">3</th>
+                                    <td>Ana Conde</td>
+                                    <td>Nueva Esparta</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">4</th>
+                                    <td>Carlos Esparragoza</td>
+                                    <td>Sucre</td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-12">
+                             <div class="card">
+                             <canvas id="myChart"></canvas>
+                             </div>
+                        </div>
+                      </div>
+                </div> 
+                </div>
+            </div>
         </div>
     </div>
-
-  
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="lib/js/principal.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.4.slim.min.js"></script>
-    
+    <script src="./lib/js/dashboard.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
 
   </body>
 </html>
--->
-<?php
-// index.php
 
-// Incluir el archivo del enrutador
-require 'router.php';
 
-// Crear una instancia del enrutador
-$router = new Router();
-
-// Definir las rutas
-$router->define([
-    '/' => 'controllers/HomeController.php',
-    '/about' => 'controllers/AboutController.php',
-    // Agrega más rutas aquí
-]);
-
-// Dirigir la solicitud
-$uri = trim($_SERVER['REQUEST_URI'], '/');
-require $router->direct($uri);
--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<!--
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link rel="stylesheet" href="lib/css/Styles.css">
+    <link rel="stylesheet" href="lib/css/StylesHome.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
@@ -13,19 +12,42 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
   </head>
   <body>
-        
-  <h1>Index</h1>
-
-
+  
+  <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-6">
+                <img src="./lib/img/logovertical.png" alt="Logo" class="logo"> 
+                <div class="card login-card">
+                    <div class="card-header">
+                        Iniciar Sesión
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="email">Usuario</label>
+                                <input type="text" class="form-control" id="usuario">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Contraseña</label>
+                                <input type="password" class="form-control" id="password">
+                            </div>
+                            <button type="button" class="btn btn-primary btn-block" id="btn-ingresar">Ingresar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <!-- Contenido de la columna derecha -->
+            </div>
+        </div>
+    </div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="lib/js/principal.js"></script>
+    <script src="lib/js/home.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.slim.min.js"></script>
     
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
 
   </body>
-</html>-->
-<?php
-require_once 'routes.php';
-?>
+</html>
