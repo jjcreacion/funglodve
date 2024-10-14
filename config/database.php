5a1ba07bfef372a807ce5521a21323b9433dd00d
Johann <jjcreacion@gmail.com>
2024-10-14 18:19:03 -0400

    14/10/2024

--- config/database.php
+++ config/database.php
@@ -1,10 +1,21 @@
 <?php
 return array(
     "driver"    =>"pgsql",
+    "host"      =>"ep-shrill-flower-a4d5x3hk-pooler.us-east-1.aws.neon.tech",
+    "user"      =>"default",
+    "pass"      =>"mR4HnV5PvILa",
+    "database"  =>"verceldb",
+    "charset"   =>"utf8"
+);
+
+/*
+return array(
+    "driver"    =>"pgsql",
     "host"      =>"localhost",
     "user"      =>"postgres",
     "pass"      =>"1234",
     "database"  =>"funglodve",
     "charset"   =>"utf8"
 );
+*/
 ?>
